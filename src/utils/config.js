65bb445ab4f9263fd38a7172af069f5fff8c9299
Ali M. Jaradat <amjaradat01@gmail.com>
2026-01-09 11:15:52 +0300

    feat: add Azure CLI plugin
    - Add auth and subscription management
    - Add VM, Storage, AKS shortcuts
    - Add App Service, Functions, ACR, Key Vault aliases

--- src/utils/config.js
+++ src/utils/config.js
@@ -6,6 +6,7 @@
 /** Plugin repository mappings - empty string indicates built-in Oh My Zsh plugin */
 export const pluginRepos = {
     'git': '', // Built-in Oh My Zsh plugin
+    'azure': '', // Azure CLI aliases
     'git-flow': '', // Built-in Oh My Zsh plugin
     'npm': '', // Built-in Oh My Zsh plugin
     'nvm': '', // Built-in Oh My Zsh plugin
